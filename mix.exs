d1ebd0106ecb6aaae6ff645ff49486b91888bdfd
Talita-8 <tlthmf1@outlook.com>
2021-05-30 00:55:56 -0300

    feat/Instala dependencias

--- mix.exs
+++ mix.exs
@@ -44,7 +44,9 @@ defmodule TheBotFinds.MixProject do
       {:telemetry_poller, "~> 0.4"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
-      {:plug_cowboy, "~> 2.0"}
+      {:plug_cowboy, "~> 2.0"},
+      {:floki, "~> 0.30.0"},
+      {:tesla, "~> 1.4.0"}
     ]
   end
 
